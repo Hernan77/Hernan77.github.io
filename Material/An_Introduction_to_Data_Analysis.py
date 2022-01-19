53314840cb030b2c35534e6da9a6b6a51401eeaf
Hernan77 <hernan.77@gmail.com>
2022-01-19 01:53:31 -0600

    Add files via upload

---
+++ Material/An_Introduction_to_Data_Analysis.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Python Data Analytics qith pandas, Numpy, and Matplotlib
+# **Fabio Nelli**
+# 
+# **Hernan Chavez 1/7/2021**
+# 
+# **Book Blurb<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).**
+
+# # Chapter One: An Introduction to Data Analysis
+# 
+# ## Data Analysis
+# Data Anlaysis<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ## Knowledge Domains of Data Analysis
+# Knowledge Domains of the Data Anlyst<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ### Computer Science
+# Knowledge Domains of the Data Anlyst > Computer Science<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ### Mathematics and Statistics
+# Knowledge Domains of the Data Anlyst > Mathematics and Statistics<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ### Machine learning and Artificial Intelligence
+# Knowledge Domains of the Data Anlyst > Machine Learning and Aritificial Intelligence<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ### Professional Fields of Application
+# Knowledge Domains of the Data Anlyst > Professional Fields of Application<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ## Understanding Nature of Data
+# Understanding the Nature of Data<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ### When the Data Become Information
+# Understanding the Nature of Data > When the Data Become Information<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ### When the Information Becomes Knowledge
+# Understanding the Nature of Data > When the Information Becomes Knowledge<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ### Types of Data
+# Understanding the Nature of Data > Types of Data<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ## The Data Analysis Process
+# The Data Anlysis Process<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# *Fig. 1.1. The data analysis process*
+# 
+# <img src="Figures/1_1.jpg" alt="The data analysis process" width="500" height="600">
+# 
+# ### Problem Definition
+# The Data Anlaysis Process > Problem Definition<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ### Data Extraction
+# The Data Anlaysis Process > Data Extraction<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ### Data Preparation
+# The Data Anlaysis Process > Data Preparation<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ### Data Exploration / Visualization
+# The Data Anlaysis Process > Data Exploration/Visualization<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ### Predictive Modeling
+# The Data Anlaysis Process > Predictive Modeling<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ### Model Validation
+# The Data Anlaysis Process > Model Validation<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ### Deployment
+# The Data Anlaysis Process > Deployment<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ## Qualitative and Quantitative Data Analysis
+# Quantitative and Qualitative Data Analysis<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# *Fig. 1.2. Quantitative and qualitative analyses*
+# 
+# <img src="Figures/1_2.jpg" alt="Quantitative and qualitative analyses" width="500" height="600">
+# 
+# ## Open Data
+# Open Data<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# Here is a list of some open data available online.
+# 
+# * Datahub (http://datahub.io/dataset)
+# * World Health Organization (https://www.who.int/data/)
+# * Data.gov (https://www.data.gov/)
+# * European Union Open Data Portal (https://data.europa.eu/en)
+# * Amazon Web Service Public Datasets (https://registry.opendata.aws/)
+# * Facebook Graph (https://developers.facebook.com/docs/graph-api)
+# * Healthdata.gov (https://www.healthdata.gov/)
+# * Google Trends (https://trends.google.com/trends/explore)
+# * Google Finance (https://www.google.com/finance/)
+# * Google Books Ngrams (https://storage.googleapis.com/books/ngrams/books/datasetsv2.html)
+# * Machine Learning Repository (http://archive.ics.uci.edu/ml/index.php)
+# 
+# As an idea of open data sources available online, you can look at the *LOD cloud duagram (https://lod-cloud.net/)*, which displays the connection of the data link among several open data sources currently available on the network.
+# 
+# ## Python and Data Analysis
+# Python and Data Analysis<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
+# 
+# ## Conclusions
+# Conclusions<br/>
+# Nelli, F. "Python Data Analytics: With Pandas." NumPy, and Matplotlib, Rome, Italy: Apress Media LLC (2018).
