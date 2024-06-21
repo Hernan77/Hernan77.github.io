0fb85df9b6146a936149ab90a27e0ffbb9fe0142
Hernan77 <hernan.77@gmail.com>
2024-06-20 19:24:13 -0500

    Add files via upload

---
+++ MURACHS_PYTHON_PROGRAMMING/USE_IDLE_TO_RUN_PROGRAMS.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # USE IDLE TO RUN PROGRAMS
+# 
+# <br>Hernan Chavez, 2024<br/>
+# Ref. Michael Urban, Joel Murach, and Mike Murach. 2016. Murach's Python Programming. Mike Murach & Associates<br>
+
+# In[1]:
+
+
+import locale
+
+# set the locale for use in currency formatting
+locale.setlocale(locale.LC_ALL, 'en_US')
+
+# display a welcome message
+print("Welcome to the Future Value Calculator")
+print()
+
+choice = "y"
+while choice.lower() == "y":
+
+    # get input from the user
+    monthly_investment = float(input("Enter monthly investment:\t"))
+    yearly_interest_rate = float(input("Enter yearly interest rate:\t"))
+    years = int(input("Enter number of years:\t\t"))
+
+    # convert yearly values to monthly values
+    monthly_interest_rate = yearly_interest_rate / 12 / 100
+    months = years * 12
+
+    # calculate the future value
+    future_value = 0
+    for i in range(months):
+        future_value = future_value + monthly_investment
+        monthly_interest_amount = future_value * monthly_interest_rate
+        future_value = future_value + monthly_interest_amount
+
+    # format and display the result
+    print("Future value:\t\t\t" + locale.currency(
+        future_value, grouping=True))
+    print()
+
+    # see if the user wants to continue
+    choice = input("Continue? (y/n): ")
+    print()
+
+print("Bye!")
+
