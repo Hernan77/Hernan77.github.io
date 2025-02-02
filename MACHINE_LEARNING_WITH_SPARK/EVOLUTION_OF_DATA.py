e9291cdd22b2fa8f6899235d1af069c4291db5bb
Hernan77 <hernan.77@gmail.com>
2025-02-02 00:13:28 -0600

    Add files via upload

---
+++ MACHINE_LEARNING_WITH_SPARK/EVOLUTION_OF_DATA.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # EVOLUTION OF DATA
+# 
+# Hernan Carlos Chavez Paura Garcia<br>
+# Feb 1st, 2025<br>
+# Singh, Pramod. Machine Learning with PySpark: With Natural Language Processing and Recommender Systems. Apress, 2018.
+
+# ## DATA GENERATION
+# 
+# <br/><br/><center>Fig. 1 Data Evolution</center>
+# 
+# <center><img src="MACHINE_LEARNING_WITH_SPARK_IMAGES/EVOLUTION_OF_DATA_1.png" width="600"></center><br/><br/>
+# 
+# <br/><br/><center>Fig. 2 Single Thread Processing</center>
+# 
+# <center><img src="MACHINE_LEARNING_WITH_SPARK_IMAGES/EVOLUTION_OF_DATA_2.png" width="600"></center><br/><br/>
+# 
+# <br/><br/><center>Fig. 3 Parallel Processing</center>
+# 
+# <center><img src="MACHINE_LEARNING_WITH_SPARK_IMAGES/EVOLUTION_OF_DATA_3.png" width="600"></center><br/><br/>
+
+# ## SPARK
+# 
+# <br/><br/><center>Fig. 4 Spark Evolution</center>
+# 
+# <center><img src="MACHINE_LEARNING_WITH_SPARK_IMAGES/EVOLUTION_OF_DATA_4.png" width="600"></center><br/><br/>
+# 
+# <br/><br/><center>Fig. 5 Spark Functionality</center>
+# 
+# <center><img src="MACHINE_LEARNING_WITH_SPARK_IMAGES/EVOLUTION_OF_DATA_5.png" width="600"></center><br/><br/>
+# 
+# 
+
+# ## SPARK CORE
+# 
+# <br/><br/><center>Fig. 6 Spark Architecture</center>
+# 
+# <center><img src="MACHINE_LEARNING_WITH_SPARK_IMAGES/EVOLUTION_OF_DATA_6.png" width="600"></center><br/><br/>
+
+# ## SPARK COMPONENTS
+# 
+# * SPARK SQL
+# * SPARK STREAMING
+# * SPARL MLIB
+# * SPARK GRAPHX/GRAPHFRAME
